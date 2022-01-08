ed312eaea025ca65d0c08401a0951bf58f917672
Abhinav Kinagi <40338035+kinagiabhinav@users.noreply.github.com>
2022-01-08 13:54:55 +0530

    Time: 9603 ms (5.01%), Space: 44.8 MB (47.03%) - LeetHub

---
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.js
@@ -0,0 +1,40 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var longestPalindrome = function(s) {
+    if(!s.length)
+        return '';
+    
+    //init i,j
+    let i = 0;
+    
+    //temp str and maxlength palindrome
+    let temp,
+        maxPalindrome = s[0];
+    
+    while(i < s.length){
+        for(let j = i; j < s.length; j++){
+            temp = s.slice(i,j+1);
+            if(isPalindrome(temp)){
+               if(maxPalindrome.length < temp.length)
+                    maxPalindrome = temp;
+                
+               // if(maxPalindrome.length === s.length)
+               //     return maxPalindrome;
+            }
+        }
+        i++;   
+    }
+    
+    return maxPalindrome;
+};
+
+function isPalindrome(sub){
+    for(let i = 0, j = sub.length - 1; i < Math.floor(sub.length/2); i++, j--){
+        if(sub[i] !== sub[j])
+            return false;
+    }
+    
+    return true;
+}
